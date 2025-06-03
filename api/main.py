262eb4ee713ec72236c13536a3ba111ffe76b64b
peter <peter.szasz333@gmail.com>
2025-06-03 13:54:07 +0200

    add reservation, user and table

--- api/main.py
+++ api/main.py
@@ -1,10 +1,93 @@
 from typing import Union
+from typing import Annotated, Optional, List
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine, select, Relationship
+from datetime import datetime
+
+class Reservation(SQLModel):
+    id: int = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    table_id: int = Field(foreign_key="table.id")
+    reservation_date: datetime
+
+    user: Optional["User"] = Relationship(back_populates="reservations")
+    table: Optional["Table"] = Relationship(back_populates="reservations")
+
+class User(SQLModel, table=True):
+    id: int= Field(default=None, primary_key=True)
+    username: str
+
+    reservations: List["Reservation"] = Relationship(back_populates="user")
+
+class Table(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    name: str
+
+    reservations: List["Reservation"] = Relationship(back_populates="user")
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
 
 app = FastAPI()
 
 
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+"""
+@app.post("/heroes/")
+def create_hero(hero: Hero, session: SessionDep) -> Hero:
+    session.add(hero)
+    session.commit()
+    session.refresh(hero)
+    return hero
+
+
+@app.get("/heroes/")
+def read_heroes(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[Hero]:
+    heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+    return heroes
+
+
+@app.get("/heroes/{hero_id}")
+def read_hero(hero_id: int, session: SessionDep) -> Hero:
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return hero
+
+
+@app.delete("/heroes/{hero_id}")
+def delete_hero(hero_id: int, session: SessionDep):
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    session.delete(hero)
+    session.commit()
+    return {"ok": True}
+"""
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
