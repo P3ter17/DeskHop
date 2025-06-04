d09e5de008a125bf000cfc0cdff5600e363a08f9
peter <peter.szasz333@gmail.com>
2025-06-04 10:17:05 +0200

    add requests reservation, user and table

--- api/main.py
+++ api/main.py
@@ -1,30 +1,32 @@
 from typing import Union
 from typing import Annotated, Optional, List
 
-from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select, Relationship
-from datetime import datetime
+from fastapi import Depends, FastAPI, HTTPException, Query, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from datetime import date, datetime
 
-class Reservation(SQLModel):
+
+
+class Reservation(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="user.id")
     table_id: int = Field(foreign_key="table.id")
-    reservation_date: datetime
-
-    user: Optional["User"] = Relationship(back_populates="reservations")
-    table: Optional["Table"] = Relationship(back_populates="reservations")
+    reservation_date: date
+# "2025-06-04"
 
 class User(SQLModel, table=True):
     id: int= Field(default=None, primary_key=True)
     username: str
 
-    reservations: List["Reservation"] = Relationship(back_populates="user")
 
 class Table(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     name: str
 
-    reservations: List["Reservation"] = Relationship(back_populates="user")
+
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -36,58 +38,136 @@ engine = create_engine(sqlite_url, connect_args=connect_args)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-
 def get_session():
     with Session(engine) as session:
         yield session
 
 
-SessionDep = Annotated[Session, Depends(get_session)]
 
+SessionDep = Annotated[Session, Depends(get_session)]
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/web/reservations/", response_class=HTMLResponse)
+async def read_item(request: Request, session: SessionDep):
+    reservations = session.exec(select(Reservation)).all()
+    return templates.TemplateResponse(
+        request=request, name="reservations.html", context={"reservations": reservations}
+    )
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
 
 
-"""
-@app.post("/heroes/")
-def create_hero(hero: Hero, session: SessionDep) -> Hero:
-    session.add(hero)
+
+@app.post("/user/")
+def create_user(user: User, session: SessionDep) -> User:
+    session.add(user)
     session.commit()
-    session.refresh(hero)
-    return hero
+    session.refresh(user)
+    return user
 
+@app.get("/user/")
+def read_users(
+        session: SessionDep,
+        offset: int = 0,
+        limit: Annotated[int, Query(le=100)] = 100,
+) -> List[User]:
+    users = session.exec(select(User).offset(offset).limit(limit)).all()
+    return users
 
-@app.get("/heroes/")
-def read_heroes(
-    session: SessionDep,
-    offset: int = 0,
-    limit: Annotated[int, Query(le=100)] = 100,
-) -> list[Hero]:
-    heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
-    return heroes
+@app.get("/user/{user_id}")
+def read_user(user_id: int, session: SessionDep) -> User:
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
 
-
-@app.get("/heroes/{hero_id}")
-def read_hero(hero_id: int, session: SessionDep) -> Hero:
-    hero = session.get(Hero, hero_id)
-    if not hero:
-        raise HTTPException(status_code=404, detail="Hero not found")
-    return hero
-
-
-@app.delete("/heroes/{hero_id}")
-def delete_hero(hero_id: int, session: SessionDep):
-    hero = session.get(Hero, hero_id)
-    if not hero:
-        raise HTTPException(status_code=404, detail="Hero not found")
-    session.delete(hero)
+@app.delete("/user/{user_id}")
+def delete_user(user_id: int, session: SessionDep):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    session.delete(user)
     session.commit()
     return {"ok": True}
-"""
+
+
+
+@app.post("/table/")
+def create_table(table: Table, session: SessionDep) -> Table:
+    session.add(table)
+    session.commit()
+    session.refresh(table)
+    return table
+
+@app.get("/table/")
+def read_tables(
+        session: SessionDep,
+        offset: int = 0,
+        limit: Annotated[int, Query(le=100)] = 100,
+) -> List[Table]:
+    tables = session.exec(select(Table).offset(offset).limit(limit)).all()
+    return tables
+
+@app.get("/table/{table_id}")
+def read_table(table_id: int, session: SessionDep) -> Table:
+    table = session.get(Table, table_id)
+    if not table:
+        raise HTTPException(status_code=404, detail="Table not found")
+    return table
+
+@app.delete("/table/{table_id}")
+def delete_table(table_id: int, session: SessionDep):
+    table = session.get(Table, table_id)
+    if not table:
+        raise HTTPException(status_code=404, detail="Table not found")
+    session.delete(table)
+    session.commit()
+    return {"ok": True}
+
+
+
+@app.post("/reservation/")
+def create_reservation(reservation: Reservation, session: SessionDep) -> Reservation:
+    reservation.reservation_date = datetime.strptime(reservation.reservation_date, "%Y-%m-%d").date()
+    session.add(reservation)
+    session.commit()
+    session.refresh(reservation)
+    return reservation
+
+@app.get("/reservation/")
+def read_reservations(
+        session: SessionDep,
+        offset: int = 0,
+        limit: Annotated[int, Query(le=100)] = 100,
+) -> List[Reservation]:
+    reservations = session.exec(select(Reservation).offset(offset).limit(limit)).all()
+    return reservations
+
+@app.get("/reservation/{reservation_id}")
+def read_reservation(reservation_id: int, session: SessionDep) -> Reservation:
+    reservation = session.get(Reservation, reservation_id)
+    if not reservation:
+        raise HTTPException(status_code=404, detail="Reservation not found")
+    return reservation
+
+@app.delete("/reservation/{reservation_id}")
+def delete_reservation(reservation_id: int, session: SessionDep):
+    reservation = session.get(Reservation, reservation_id)
+    if not reservation:
+        raise HTTPException(status_code=404, detail="Reservation not found")
+    session.delete(reservation)
+    session.commit()
+    return {"ok": True}
+
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
